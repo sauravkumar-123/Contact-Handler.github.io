65e96049c655db8f178a20ebc025ea121f573c54
Saurav Kumar <krsaurav.097@gmail.com>
2020-12-09 21:32:04 +0530

    ->Dependicences Added (1st commit)

---
+++ SmartConatactManagement/src/main/java/com/smartcontact/start/SmartConatactManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smartcontact.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConatactManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartConatactManagementApplication.class, args);
+	}
+
+}
---
+++ SmartConatactManagement/src/test/java/com/smartcontact/start/SmartConatactManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcontact.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartConatactManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
